b65edac77fb30a4ca7cef0fad63702244776b5f3
umnomequalqu <csa16-6436@colegiosantoantonio.com.br>
2023-04-18 16:31:01 -0300

    quarto

--- client/src/components/Titulo.jsx
+++ client/src/components/Titulo.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { ModeloTitulo,TituloData,TituloNome} from "./styles/styles";
 function Titulo(){
+    const [data, setData]= useState();
+    useEffect(()=>{
+        const dataAtual = new Date();
+        const dataFormato = {weekday: 'long',day: 'numeric',month: 'long'}
+        const dataFinal = dataAtual.toLocaleDateString("pt-br", dataFormato)
+        setData(dataFinal)
+    })
     return(
         <ModeloTitulo>
             <TituloNome>cadastro de tarefas</TituloNome>
-            <TituloData>Terça Feira, 18 de abril</TituloData>
+            <TituloData>{data}</TituloData>
         </ModeloTitulo>
     )
 }
