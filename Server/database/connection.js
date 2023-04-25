aae16bddd4fbb6f6a1a55346b1524a83b3910bba
umnomequalqu <csa16-6436@colegiosantoantonio.com.br>
2023-04-25 16:17:52 -0300

    connect

--- Server/database/connection.js
+++ Server/database/connection.js
@@ -2,3 +2,17 @@ const mongoose = require("mongoose")
 
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
+
+const connect = ()=>{
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@aula-banco.mtz4vkx.mongodb.net/test?retryWrites=true&w=majority`)
+    const connection = mongoose.connection;
+
+    connection.on("error",()=>{
+        console.log("Erro ao conectar com o mongoDB")
+    })
+
+    connection.on("open",()=>{
+        console.log("Conectado ao mongoDB")
+    })
+}
+connect()
