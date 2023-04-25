a5dc8c8b13356886ab3a1a96de8b82b6f006095e
umnomequalqu <csa16-6436@colegiosantoantonio.com.br>
2023-04-25 16:29:05 -0300

    conectado

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,14 @@
-console.log("opa")
+const express = require('express')
+const cors = require ('cors')
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors())
+app.use(express.json())
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando na porta: ${port}`)
+})
+require("./database/connection")
--- Server/database/connection.js
+++ Server/database/connection.js
@@ -4,7 +4,7 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
 const connect = ()=>{
-    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@aula-banco.mtz4vkx.mongodb.net/test?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@aula-banco.mtz4vkx.mongodb.net/?retryWrites=true&w=majority`)
     const connection = mongoose.connection;
 
     connection.on("error",()=>{
