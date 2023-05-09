6d816c6631f10b11fa5ca05f8b2539f23f1649e9
umnomequalqu <csa16-6436@colegiosantoantonio.com.br>
2023-05-09 16:35:05 -0300

    Criptografia

--- Server/controllers/AuthRegisterUserController.js
+++ Server/controllers/AuthRegisterUserController.js
@@ -1,10 +1,12 @@
-const User = require('../models/USUARIO')
-const bcrypt = require ('bcrypt');
+const User = require('../models/User')
+
+const bcrypt = require('bcrypt');
+
 module.exports = class AuthRegisterUserController{
   static async init(req,res){
     res.send({message: "Bem vindo a nossa api!"})
   }
-  static async registeruser(req,res){
+  static async registerUser(req,res){
     const {
       name,
       email,
@@ -17,25 +19,26 @@ module.exports = class AuthRegisterUserController{
     if(req.file){
       image = req.file.filename
     }
-    if (!name){
+    if(!name){
       return res.status(422).json({message: "o nome é obrigatório"})
     }
-    if (!email){
+    if(!email){
       return res.status(422).json({message: "o email é obrigatório"})
     }
-    if (!age){
+    if(!age){
       return res.status(422).json({message: "a idade é obrigatória"})
     }
-    if (!password){
+    if(!password){
       return res.status(422).json({message: "a senha é obrigatória"})
     }
     if(confirmPassword != password){
-      return res.status(422).json({message: "a senha ou o email está errado"})
+      return res.status(422).json({message: "As senhas não são iguais"})
     }
 
-    const userExit = await User.findOne({email: email})
+    const userExist = await User.findOne({email: email})
+
     if(userExist){
-      return res.status(422).json({message: "já há uma conta com esse email"})
+      return res.status(422).json({message: "Já tem um usuário com teu email"})
     }
     const hash = await bcrypt.genSalt(12)
 
@@ -48,6 +51,11 @@ module.exports = class AuthRegisterUserController{
       image,
       password: passwordHash
     })
-    
+    try {
+      await user.save()
+      res.status(201).json({message: "Usuário cadastrado com sucesso",user})
+    } catch (error) {
+      res.status(500).json({message: "Ocorreu um erro ao cadastrar o usuário, tente novamente mais tarde"})
+    }
   }
 }
