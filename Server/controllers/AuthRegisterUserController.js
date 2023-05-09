cec96c8c485400eab80b0843c33ae0d63aae76ff
umnomequalqu <csa16-6436@colegiosantoantonio.com.br>
2023-05-09 16:02:39 -0300

    validacao da requisicao

--- Server/controllers/AuthRegisterUserController.js
+++ Server/controllers/AuthRegisterUserController.js
@@ -5,6 +5,32 @@ module.exports = class AuthRegisterUserController{
     res.send({message: "Bem vindo a nossa api!"})
   }
   static async registeruser(req,res){
-    
+    const {
+      name,
+      email,
+      age,
+      password,
+      confirmPassword
+    }= req.body
+
+    let image = ""
+    if(req.file){
+      image = req.file.filename
+    }
+    if (!name){
+      return res.status(422).json({message: "o nome é obrigatório"})
+    }
+    if (!email){
+      return res.status(422).json({message: "o email é obrigatório"})
+    }
+    if (!age){
+      return res.status(422).json({message: "a idade é obrigatória"})
+    }
+    if (!password){
+      return res.status(422).json({message: "a senha é obrigatória"})
+    }
+    if(confirmPassword != password){
+      return res.status(422).json({message: "a senha ou o email está errado"})
+    }
   }
 }
