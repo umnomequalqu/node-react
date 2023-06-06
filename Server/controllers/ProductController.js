978241b9c2ce1af63b7194b1c409e84fc54b463d
umnomequalqu <csa16-6436@colegiosantoantonio.com.br>
2023-06-06 16:16:00 -0300

    delete ok

--- Server/controllers/ProductController.js
+++ Server/controllers/ProductController.js
@@ -21,4 +21,33 @@ module.exports= class ProductsController{
             res.status(500).json({message: "Erro ao pegar todos os produtos."})
         }
     }
-}
+    static async editProduct(req,res){
+        try {
+            const {nome,price,desc,disponivel} = req.body;
+            const produto = await Product.findByIdAndUpdate(
+                req.params.id,
+                {nome,price,desc,disponivel},
+                {new: true}
+            );
+                if(!produto){
+                    return res.status(404).json({message: "este produto não existe"})
+                }
+                res.json(produto);
+        }catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Erro ao editar o produto."})
+        }
+        }
+        static async deleteProduto(req,res){
+            try{
+                const produto = await Product.findByIdAndRemove(req.params.id)
+                if(!produto){
+                    return res.status(404).json({message: "Não existe um produto com esse id"})
+                }
+                res.json({message: "Produto deletado com sucesso!"})
+            }catch (error) {
+                console.log(error);
+                res.status(500).json({message: "Erro ao deletar o produto."})
+            }
+        }
+    }
