e28b0c1e86875f3a84c00bbf5937baf20eec2d97
umnomequalqu <csa16-6436@colegiosantoantonio.com.br>
2023-06-13 15:22:33 -0300

    checkbox

--- client/src/components/pages/ProdutosForm.jsx
+++ client/src/components/pages/ProdutosForm.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 
 function ProdutosForm(){
+    const [disponivel,setDisponivel] = useState(false);
+
+    const handleCheckboxChange = (event)=>{
+        setDisponivel(event.target.checked);
+    }
     return(
         <div>
-
+            <h1> Cadastre seu Produto</h1>
+            <div>
+                <form>
+                    <label htmlFor="nome">Name</label>
+                    <input type="text" id="nome" name="nome"/>
+                    <label htmlFor="desc">Descrição </label>
+                    <input type="text" id="desc" name="desc"/>
+                    <label htmlFor="price">Price</label>
+                    <input type="number" id="price" name="price"/>
+                    <label htmlFor="disponivel">Produto Disponivel:</label>
+                    <input
+                        type="checkbox"
+                        name="disponivel"
+                        id="disponivel"/>
+                    <label htmlFor="nDisponivel">Produto NãoDisponivel:</label>
+                    <input
+                        type="checkbox"
+                        name="nDisponivel"
+                        id="nDisponivel"/>
+                </form>
+            </div>
         </div>
     )
 }
